04c2a9d4e2e5a53842d6d9b42a36e7d391ab8e8b
honeychaudharyc <honeychaudhary.c@eginnovations.com>
2024-05-28 10:56:30 +0530

    updating tests

--- exporter/eginnovations/factory_test.go
+++ exporter/eginnovations/factory_test.go
@@ -5,12 +5,19 @@ package eginnovations
 
 import (
 	"context"
+	"path/filepath"
 	"testing"
 
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/eginnovations/internal/metadata"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenttest"
-	"go.opentelemetry.io/collector/confmap"
+	"go.opentelemetry.io/collector/config/configcompression"
+	"go.opentelemetry.io/collector/config/configgrpc"
+	"go.opentelemetry.io/collector/config/configopaque"
+	"go.opentelemetry.io/collector/config/configtls"
+	"go.opentelemetry.io/collector/confmap/confmaptest"
 	"go.opentelemetry.io/collector/exporter/exportertest"
 )
 
@@ -21,10 +28,44 @@ func TestCreateDefaultConfig(t *testing.T) {
 }
 
 func TestCreateUnmarshalConfig(t *testing.T) {
-	factory := NewFactory()
-	cfg := factory.CreateDefaultConfig()
-	assert.NoError(t, component.UnmarshalConfig(confmap.New(), cfg))
-	assert.Equal(t, factory.CreateDefaultConfig(), cfg)
+	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
+	require.NoError(t, err)
+	tests := struct {
+		id       component.ID
+		expected component.Config
+	}{
+		id: component.NewIDWithName(metadata.Type, "all"),
+		expected: &Config{
+			UserID: "user1",
+			Token:  "xxxxxxxxx",
+			ClientConfig: configgrpc.ClientConfig{
+				Endpoint:    "eginnovations.com",
+				Compression: configcompression.TypeGzip,
+				TLSSetting: configtls.ClientConfig{
+					Config:             configtls.Config{},
+					Insecure:           false,
+					InsecureSkipVerify: false,
+					ServerName:         "",
+				},
+				ReadBufferSize:  0,
+				WriteBufferSize: 0,
+				WaitForReady:    false,
+				Headers: map[string]configopaque.String{
+					"hdr1": "value1",
+				},
+				BalancerName: "",
+			},
+		},
+	}
+
+	t.Run(tests.id.String(), func(t *testing.T) {
+		factory := NewFactory()
+		cfg := factory.CreateDefaultConfig()
+		sub, err := cm.Sub(tests.id.String())
+		require.NoError(t, err)
+		require.NoError(t, component.UnmarshalConfig(sub, cfg))
+	})
+
 }
 
 func TestCreateTestTraceExporter(t *testing.T) {
